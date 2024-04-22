e52885166b74550a57efec5d6fe8f577ac10df89
Lorenzo Rota <lorenzokaide@gmail.com>
2024-04-22 17:21:49 +0200

    feat: began implementing project selection page

--- my-app/src/routes/+page.server.ts
+++ my-app/src/routes/+page.server.ts
@@ -1,36 +1,70 @@
 import { projects } from '$db/projects';
 import type { PageServerLoad } from './$types';
+import type { State } from '@vincjo/datatables/remote';
 
-type ProjectDictionary = {
-	[key: string]: string;
-};
-
-type LoadReturnType = {
-	projects: ProjectDictionary;
-};
-
-export const load: PageServerLoad = async function (): Promise<LoadReturnType> {
-	const projectNames = await projects
-		.find(
-			{},
-			{
-				limit: 50,
-				projection: {
-					name: 1
-				}
-			}
-		)
-		.toArray();
-
-	const projectDictionary: ProjectDictionary = projectNames.reduce(
-		(dict: ProjectDictionary, item: any) => {
-			dict[item._id.toString()] = item.name;
-			return dict;
+export const load: PageServerLoad = async function (event): Promise<any> {
+	const { url } = event;
+	const state: State = {
+		pageNumber: parseInt(url.searchParams.get('page') || '1'),
+		rowsPerPage: parseInt(url.searchParams.get('limit') || '25'),
+		sort: {
+			orderBy: url.searchParams.get('sort') || '_id',
+			direction: 'asc' //url.searchParams.get('order')
 		},
-		{}
-	);
+		filters: [],
+		search: url.searchParams.get('search') || '',
+		offset: parseInt(url.searchParams.get('offset') || '0'),
+		setTotalRows: () => {},
+		sorted: undefined
+	};
+
+	const { query, options } = getParams(state);
+
+	const fetchProjectInfos = async () => {
+		const projectInfos = await projects.find(query, options).toArray();
+
+		return projectInfos.map((project) => ({
+			...project,
+			_id: project._id.toString() // Convert _id to a string
+		}));
+	};
+
+	const fetchProjectNum = async () => {
+		return await projects.countDocuments();
+	};
 
 	return {
-		projects: projectDictionary
+		projectInfos: await fetchProjectInfos(),
+		projectNum: await fetchProjectNum()
 	};
+};
+
+const getParams = (state: State) => {
+	const { pageNumber, rowsPerPage, sort, filters, search } = state;
+
+	const query: any = {};
+	const options: any = {};
+
+	if (pageNumber && rowsPerPage) {
+		options.skip = (pageNumber - 1) * rowsPerPage;
+		options.limit = rowsPerPage;
+	}
+
+	if (sort) {
+		options.sort = {
+			[sort.orderBy as keyof any]: sort.direction === 'asc' ? 1 : -1
+		};
+	}
+
+	if (filters) {
+		filters.forEach(({ filterBy, value }) => {
+			query[filterBy] = value;
+		});
+	}
+
+	if (search) {
+		query.$text = { $search: search };
+	}
+
+	return { query, options };
 };
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,84 +1,37 @@
 <script lang="ts">
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import type { PageData } from './$types';
-	import { writable } from 'svelte/store';
 
 	export let data: PageData;
-	$: ({ projects } = data);
+	$: ({ projectInfos, projectNum } = data);
 
-	const selectedProjectId = writable('');
-
-	$: selectedProject = $selectedProjectId ? projects[$selectedProjectId] : null;
-	$: isAddingNewProject = $selectedProjectId === '';
-
-	function selectProject(projectId: string) {
-		selectedProjectId.set(projectId);
-	}
-
-	function selectAddNewProject() {
-		selectedProjectId.set('');
-	}
-
-	import { Autocomplete } from '@skeletonlabs/skeleton';
-	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
-
-	let inputDemo = '';
-
-	const flavorOptions: AutocompleteOption<string>[] = [
-		{ label: 'Vanilla', value: 'vanilla', keywords: 'plain, basic', meta: { healthy: false } },
-		{ label: 'Chocolate', value: 'chocolate', keywords: 'dark, white', meta: { healthy: false } },
-		{ label: 'Strawberry', value: 'strawberry', keywords: 'fruit', meta: { healthy: true } },
-		{
-			label: 'Neapolitan',
-			value: 'neapolitan',
-			keywords: 'mix, strawberry, chocolate, vanilla',
-			meta: { healthy: false }
-		},
-		{ label: 'Pineapple', value: 'pineapple', keywords: 'fruit', meta: { healthy: true } },
-		{ label: 'Peach', value: 'peach', keywords: 'fruit', meta: { healthy: true } }
-	];
-
-	function onFlavorSelection(event: CustomEvent<AutocompleteOption<string>>): void {
-		inputDemo = event.detail.label;
-	}
-
-	let searchFocused = false;
+	import Datatable from '$lib/components/server/Datatable.svelte';
 </script>
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-			<svelte:fragment slot="lead">
-				<input
-					class="input w-72"
-					type="search"
-					name="demo"
-					bind:value={inputDemo}
-					placeholder="Search project..."
-					on:focus={() => (searchFocused = true)}
-					on:blur={() => (searchFocused = false)}
-				/>
-			</svelte:fragment>
-			Light LCA tool - Project Selection
-			<svelte:fragment slot="trail">
-				<button>Logout</button>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-
-	<svelte:fragment slot="sidebarLeft">
-		<div class="card h-full w-80 max-w-sm overflow-y-auto p-4" tabindex="-1">
-			<Autocomplete
-				bind:input={inputDemo}
-				options={flavorOptions}
-				on:selection={onFlavorSelection}
-			/>
+		<div class="bg-gray-200">
+			<AppBar
+				gridColumns="grid-cols-3"
+				slotDefault="place-self-center"
+				slotTrail="place-content-end"
+			>
+				<svelte:fragment slot="lead">Admin-user pane</svelte:fragment>
+				Light LCA tool - Project Selection
+				<svelte:fragment slot="trail">
+					<button>Logout</button>
+				</svelte:fragment>
+			</AppBar>
 		</div>
 	</svelte:fragment>
 
-	<div class="page-content"></div>
+	<div class="px-12 py-7">
+		<Datatable {projectInfos} {projectNum} />
+	</div>
 
 	<svelte:fragment slot="pageFooter">
-		<AppBar>Page footer</AppBar>
+		<div class="bg-gray-400">
+			<AppBar>Page footer</AppBar>
+		</div>
 	</svelte:fragment>
 </AppShell>
